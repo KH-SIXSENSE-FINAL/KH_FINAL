41f45d651a1c98261f46af25e48a618b2cbb3aec
cha jae hyeon <chajy9566@gmail.com>
2023-09-01 09:25:31 +0900

    0901

--- FinalEx/demo/src/main/java/com/kh/muzip/board/model/service/BoardServiceImpl.java
+++ FinalEx/demo/src/main/java/com/kh/muzip/board/model/service/BoardServiceImpl.java
@@ -1,17 +1,46 @@
 package com.kh.muzip.board.model.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.kh.muzip.board.model.dao.BoardDao;
+import com.kh.muzip.board.model.vo.Attachment;
+import com.kh.muzip.board.model.vo.Board;
+import com.kh.muzip.common.Utils;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class BoardServiceImpl implements BoardService{
 		
 	@Autowired
 	private BoardDao boardDao;
 	
-	
-	
+	@Transactional(rollbackFor = {Exception.class})
+	@Override
+	public int insertBoard(Board b, List<Attachment> atList, String serverFolderPath, String webPath) throws Exception {
 		
+		b.setBoardContent(Utils.XSSHandling(b.getBoardContent()));
+		b.setBoardContent(Utils.newLineHandling(b.getBoardContent()));
+		
+		int boardNo = boardDao.insertBoard(b);
+		log.info("보드넘버",boardNo);
+		int result = 0;
+		if(boardNo > 0 && !atList.isEmpty()) {
+			for(Attachment at  : atList) {
+				at.setBoardNo(boardNo+"");
+				at.setFilePath(webPath);
+				log.info("아이템 {}"+at);
+			}
+			result = boardDao.insertAttachmentList(atList);
+			if(result != atList.size()) { // 이미지 삽입 실패시 강제 예외발생
+				throw new Exception("예외발생");
+			}
+		}
+		return result;
+	}
 }
