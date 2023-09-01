cb25c6106f08b35040715228ac197b989190028c
DESKTOP-L02DVUG\user2 <wjdxorguswl@gmail.com>
2023-09-01 17:08:00 +0900

    음악 등록 기능

--- FinalEx/demo/src/main/java/com/kh/muzip/music/controller/MusicController.java
+++ FinalEx/demo/src/main/java/com/kh/muzip/music/controller/MusicController.java
@@ -1,19 +1,27 @@
 package com.kh.muzip.music.controller;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.ServletContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.kh.muzip.board.model.vo.Attachment;
+import com.kh.muzip.common.Utils;
 import com.kh.muzip.music.model.service.MusicService;
 import com.kh.muzip.music.model.vo.Music;
+import com.kh.muzip.music.model.vo.MusicFile;
 import com.kh.muzip.music.model.vo.Playlist;
 
 import lombok.extern.slf4j.Slf4j;
@@ -43,15 +51,69 @@ public class MusicController {
 			@RequestBody Map<String, List<String>> requestData
 			){
 		List<String> genreList = requestData.get("genre");
-//		List<String> genreList = new ArrayList<String>();
-//		for(int i = 0; i < genre.size(); i++) {			
-//			genreList.add((String) genre.get(i));
-//		}
 		
 		ArrayList<ArrayList<Music>> recommendList = musicService.selectRecommendList(genreList);
-		log.info("recommendList == {}", recommendList);
+		
 		return recommendList;
 		
 	}
+	
+	@Autowired
+	private ServletContext application;
+	
+	@CrossOrigin(origins = "http://localhost:3000")
+	@PostMapping(value ="/insertMusic", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public int insertMusic(
+			@RequestParam("cover") MultipartFile cover,
+			@RequestParam("music") MultipartFile music,
+			@RequestParam("title") String title, 
+			@RequestParam("artist") String artist,
+			@RequestParam("lyrics") String lyrics,
+			@RequestParam("genre") String genre
+			){
+
+		Music m = new Music();
+		m.setMusicTitle(title);
+		m.setMusicArtist(artist);
+		m.setMusicLyrics(lyrics);
+		m.setGenre(genre);
+		
+		String imgaeWebPath="/resources/image/";
+		String mp3WebPath="/resources/mp3/";
+		String imageServerFolderPath = application.getRealPath(imgaeWebPath);
+		String mp3ServerFolderPath = application.getRealPath(mp3WebPath);
+		
+		// 디렉토리 생성, 해당 디렉토리가 존재하지 않는다면 생성
+		File dir1 = new File(imageServerFolderPath);
+		File dir2 = new File(mp3ServerFolderPath);
+		if(!dir1.exists()) dir1.mkdirs();
+		if(!dir2.exists()) dir2.mkdirs();
+		
+		MusicFile musicFile = new MusicFile();
+		
+		if(!cover.isEmpty()) {			
+			// 파일명 재정의 + 저장
+			String changeName = Utils.saveFile(cover, imageServerFolderPath);
+			musicFile.setCoverChangeName(changeName);
+			musicFile.setCoverOriginName(cover.getOriginalFilename());
+		};
+		if(!music.isEmpty()) {
+			String changeName = Utils.saveFile(music, mp3ServerFolderPath);
+			musicFile.setMusicChangeName(changeName);
+			musicFile.setMusicOriginName(music.getOriginalFilename());
+		}
+			
+		int result = 0;
+		
+		try {
+			result = musicService.insertMusic(m, musicFile, 
+					imageServerFolderPath, imgaeWebPath, mp3ServerFolderPath, mp3WebPath);
+		} catch (Exception e) {
+			log.error("error = {}", e.getMessage());
+		}
+		
+		return result;		
+	}
+
 
 }
--- FinalEx/demo/src/main/java/com/kh/muzip/music/model/dao/MusicDao.java
+++ FinalEx/demo/src/main/java/com/kh/muzip/music/model/dao/MusicDao.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.kh.muzip.music.model.vo.Music;
+import com.kh.muzip.music.model.vo.MusicFile;
 import com.kh.muzip.music.model.vo.Playlist;
 
 import lombok.extern.slf4j.Slf4j;
@@ -21,7 +22,7 @@ public class MusicDao {
 	
 	public ArrayList<Playlist> selectPlaylist(int userNo){
 		ArrayList<Playlist> playlist = new ArrayList<Playlist>();
-		List<Playlist> list = session.selectList("music.selectPlaylist", userNo);
+		List<Playlist> list = session.selectList("musicMapper.selectPlaylist", userNo);
 		for(int i = 0; i < list.size(); i++) {
 			playlist.add(list.get(i));
 		}
@@ -31,24 +32,24 @@ public class MusicDao {
 	public ArrayList<ArrayList<Music>> selectRecommendList(List genreList){
 		ArrayList<Music> genreArrayList = new ArrayList<Music>();
 		if(genreList.size() == 0) {			
-			List<Music> genreRecommendList = session.selectList("music.nonGenreRecommendSongs");
+			List<Music> genreRecommendList = session.selectList("musicMapper.nonGenreRecommendSongs");
 			for(int i = 0; i < genreRecommendList.size(); i++) {
 				genreArrayList.add(genreRecommendList.get(i));
 			}
 		}else {
-			List<Music> genreRecommendList = session.selectList("music.genreRecommendSongs", genreList);
+			List<Music> genreRecommendList = session.selectList("musicMapper.genreRecommendSongs", genreList);
 			for(int i = 0; i < genreRecommendList.size(); i++) {
 				genreArrayList.add(genreRecommendList.get(i));
 			}
 		}
 		
 		ArrayList<Music> hourArrayList = new ArrayList<Music>();
-		List<Music> hourList = session.selectList("music.hourRecommendSongs");
+		List<Music> hourList = session.selectList("musicMapper.hourRecommendSongs");
 		for(int i = 0; i < hourList.size(); i++) {
 			hourArrayList.add(hourList.get(i));
 		}
 		ArrayList<Music> latestArrayList = new ArrayList<Music>();
-		List<Music> latestList = session.selectList("music.latestRecommendSongs");
+		List<Music> latestList = session.selectList("musicMapper.latestRecommendSongs");
 		for(int i = 0; i < latestList.size(); i++) {
 			latestArrayList.add(latestList.get(i));
 		}
@@ -58,6 +59,21 @@ public class MusicDao {
 		recommendList.add(latestArrayList);
 		
 		return recommendList;
+	}
+	
+	public String insertMusic(Music m) {
+		int result = 0;
+		String resultString = "";
+		result = session.insert("musicMapper.insertMusic", m);
+		if(result > 0) {
+			// selectKey 이용하여 셋팅한 musicNo값을 가져옴
+			resultString = m.getMusicNo();
+		}
+		return resultString;
+	}
+	
+	public int insertMusicFile(MusicFile musicFile) {
+		return session.insert("musicMapper.insertMusicFile", musicFile);
 	}
 
 }
