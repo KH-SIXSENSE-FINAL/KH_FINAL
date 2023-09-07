412a933133b879cedd742bc1ffd1cbc5d5d4a415
ajk6757 <129792901+ajk6757@users.noreply.github.com>
2023-09-07 14:28:14 +0900

    푸쉬

--- FinalEx/demo/src/main/java/com/kh/muzip/chat/vo/ChatMessage.java
+++ FinalEx/demo/src/main/java/com/kh/muzip/chat/vo/ChatMessage.java
@@ -7,12 +7,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "CHAT_MSG")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -20,6 +22,7 @@ public class ChatMessage {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column()
 	private int msgNo;
 	
 	private String chatroomNo;
