17fbafc5348c55a675d67de1f303f62c763e8ebe
doumybest <116059254+doumybest@users.noreply.github.com>
2023-09-12 16:51:50 +0900

    Update WebSocketController.java

--- FinalEx/demo/src/main/java/com/kh/muzip/chat/webSocket/WebSocketController.java
+++ FinalEx/demo/src/main/java/com/kh/muzip/chat/webSocket/WebSocketController.java
@@ -1,292 +1,87 @@
-package com.kh.muzip.music.controller;
+package com.kh.muzip.chat.webSocket;
 
-import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.sql.Date;
 import java.util.Map;
-
-import javax.servlet.ServletContext;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.user.SimpUser;
+import org.springframework.messaging.simp.user.SimpUserRegistry;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import com.kh.muzip.board.model.vo.Attachment;
-import com.kh.muzip.common.Utils;
-import com.kh.muzip.music.model.service.MusicService;
-import com.kh.muzip.music.model.vo.Music;
-import com.kh.muzip.music.model.vo.MusicFile;
-import com.kh.muzip.music.model.vo.Playlist;
+import com.kh.muzip.chat.service.AlarmService;
+import com.kh.muzip.chat.service.ChatService;
+import com.kh.muzip.chat.vo.Alarm;
+import com.kh.muzip.chat.vo.ChatMessage;
 
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * @author user2
- *
- */
-@RestController
+@Controller
 @Slf4j
-public class MusicController {
+public class WebSocketController extends TextWebSocketHandler{
+    
+    @Autowired
+    public ChatService service;
+    
+    @Autowired
+    private AlarmService alarmService;
+    
+	@MessageMapping("/chat") // 클라이언트에서 "/chat"로 메시지를 보내면 이 핸들러가 호출
+	@SendTo("/chat/chatget") // 이 핸들러가 반환하는 메시지는 "/chat/chatget" 주제로 브로드캐스트
+	public ChatMessage handleChatMessage(@Payload ChatMessage message) {
+        // 받은 메시지를 저장 로직
+		message.setCreateDate(new Date(System.currentTimeMillis()));
+		log.info("받아온 메세지는 어디로 갔을까?=={}",message);
+		int result = service.insertMsg(message);
+		
+		return message;
+    }
 	
-	@Autowired
-	private MusicService musicService;
-	
-	@CrossOrigin(origins = "http://localhost:3000")
-	@GetMapping("/myPlaylist")
-	public ArrayList<Playlist> myPlaylist(
-			@RequestParam("userNo") String userNo
-			){
+	@MessageMapping("/alarm")
+	@SendTo("/alarm/alarmget")
+	public Alarm handleAlarm(@Payload Alarm alarm) {
 		
-		ArrayList<Playlist> playlist = musicService.selectPlaylist(Integer.parseInt(userNo));
+		String alarmNo = "";
 		
-		return playlist;
-		
-	}
-	
-	@CrossOrigin(origins = "http://localhost:3000")
-	@PostMapping("/recommendList")
-	public ArrayList<ArrayList<Music>> recommendList(
-			@RequestBody Map<String, List<String>> requestData
-			){
-		List<String> genreList = requestData.get("genre");
-		
-		ArrayList<ArrayList<Music>> recommendList = musicService.selectRecommendList(genreList);
-		
-		return recommendList;
-	}
-	
-	@Autowired
-	private ServletContext application;
-	
-	@CrossOrigin(origins = "http://localhost:3000")
-	@PostMapping(value ="/insertMusic", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public int insertMusic(
-			@RequestParam("cover") MultipartFile cover,
-			@RequestParam("music") MultipartFile music,
-			@RequestParam("title") String title, 
-			@RequestParam("artist") String artist,
-			@RequestParam("lyrics") String lyrics,
-			@RequestParam("genre") String genre
-			){
-
-		Music m = new Music();
-		m.setMusicTitle(title);
-		m.setMusicArtist(artist);
-		m.setMusicLyrics(lyrics);
-		m.setGenre(genre);
-		
-		String imgaeWebPath="/resources/image/";
-		String mp3WebPath="/resources/mp3/";
-		String imageServerFolderPath = application.getRealPath(imgaeWebPath);
-		String mp3ServerFolderPath = application.getRealPath(mp3WebPath);
-		
-		// 디렉토리 생성, 해당 디렉토리가 존재하지 않는다면 생성
-		File dir1 = new File(imageServerFolderPath);
-		File dir2 = new File(mp3ServerFolderPath);
-		if(!dir1.exists()) dir1.mkdirs();
-		if(!dir2.exists()) dir2.mkdirs();
-		
-		MusicFile musicFile = new MusicFile();
-		
-		if(!cover.isEmpty()) {			
-			// 파일명 재정의 + 저장
-			String changeName = Utils.saveFile(cover, imageServerFolderPath);
-			musicFile.setCoverChangeName(changeName);
-			musicFile.setCoverOriginName(cover.getOriginalFilename());
-		};
-		if(!music.isEmpty()) {
-			String changeName = Utils.saveFile(music, mp3ServerFolderPath);
-			musicFile.setMusicChangeName(changeName);
-			musicFile.setMusicOriginName(music.getOriginalFilename());
-		}
+		if(alarm.getAlarmKind().equals("chat")) {
 			
-		int result = 0;
-		
-		try {
-			result = musicService.insertMusic(m, musicFile, 
-					imageServerFolderPath, imgaeWebPath, mp3ServerFolderPath, mp3WebPath);
-		} catch (Exception e) {
-			log.error("error = {}", e.getMessage());
-		}
-		
-		return result;		
-	}
-	
-	
-	
-	/**
-	 * @param musicNo
-	 * @param cover
-	 * @param music
-	 * @param title
-	 * @param artist
-	 * @param lyrics
-	 * @param genre
-	 * @return
-	 * 나중에 관리자 기능 할때 update 잘 되나 확인!!!
-	 */
-	@CrossOrigin(origins = "http://localhost:3000")
-	@PostMapping(value ="/updateMusic", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public int updateMusic(
-			@RequestParam("musicNo") String musicNo,
-			@RequestParam("cover") MultipartFile cover,
-			@RequestParam("music") MultipartFile music,
-			@RequestParam("title") String title, 
-			@RequestParam("artist") String artist,
-			@RequestParam("lyrics") String lyrics,
-			@RequestParam("genre") String genre
-			){
-
-		Music m = new Music();
-		m.setMusicNo(musicNo);
-		m.setMusicTitle(title);
-		m.setMusicArtist(artist);
-		m.setMusicLyrics(lyrics);
-		m.setGenre(genre);
-		
-		String imgaeWebPath="/resources/image/";
-		String mp3WebPath="/resources/mp3/";
-		String imageServerFolderPath = application.getRealPath(imgaeWebPath);
-		String mp3ServerFolderPath = application.getRealPath(mp3WebPath);
-		
-		// 해당 디렉토리가 존재하지 않는다면 생성
-		File dir1 = new File(imageServerFolderPath);
-		File dir2 = new File(mp3ServerFolderPath);
-		if(!dir1.exists()) dir1.mkdirs();
-		if(!dir2.exists()) dir2.mkdirs();
-		
-		MusicFile musicFile = new MusicFile();
-		
-		if(!cover.isEmpty()) {
-			// 파일명 재정의 + 저장
-			String changeName = Utils.saveFile(cover, imageServerFolderPath);
-			musicFile.setCoverChangeName(changeName);
-			musicFile.setCoverOriginName(cover.getOriginalFilename());
-		};
-		if(!music.isEmpty()) {
-			String changeName = Utils.saveFile(music, mp3ServerFolderPath);
-			musicFile.setMusicChangeName(changeName);
-			musicFile.setMusicOriginName(music.getOriginalFilename());
-		}
+			alarm.setAlarmMessage("새로운 채팅 메시지가 있습니다.");
+			alarmNo = alarmService.insertChatAlarm(alarm);
 			
-		int result = 0;
-		
-		try {
-			result = musicService.updateMusic(m, musicFile,
-					imageServerFolderPath, imgaeWebPath, mp3ServerFolderPath, mp3WebPath);
-		} catch (Exception e) {
-			log.error("error = {}", e.getMessage());
+		}else if(alarm.getAlarmKind().equals("follow")) {
+			
+			String userId = alarm.getSenderNo();
+			String senderUserNo = alarmService.searchUserNo(userId);
+			String receiverUserNo = alarmService.searchUserNo(alarm.getReceiverNo());
+			
+			alarm.setSenderNo(senderUserNo);
+			alarm.setReceiverNo(receiverUserNo);
+			alarm.setAlarmMessage(userId+"님이 당신을 팔로우하였습니다.");
+			
+			alarmNo = alarmService.insertFollowAlarm(alarm);
+		}else if(alarm.getAlarmKind().equals("reply")) {
+			
+			String userId = alarmService.getUserId(alarm.getSenderNo());
+			String receiverNo = alarmService.getReplyReceiverNo(alarm.getAlarmPath());
+			alarm.setReceiverNo(receiverNo);
+			alarm.setAlarmMessage(userId+"님이 게시글에 댓글을 달았습니다.");
+			alarmNo = alarmService.insertReplyAlarm(alarm);
+			
 		}
 		
-		return result;		
-	}
-	
-	@CrossOrigin(origins = "http://localhost:3000")
-	@GetMapping(value ="/deleteMusic", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public int deleteMusic(@RequestParam("musicNo") String musicNo) {
+		alarm.setAlarmNo(alarmNo+"");
 		
-		return musicService.deleteMusic(musicNo);
-	}
+		return alarm;
+        
+    }
 	
 	
-	@CrossOrigin(origins = "http://localhost:3000")
-	@GetMapping("/insertPlaylist")
-	public int insertPlaylist(
-			@RequestParam("listName") String playlistName,
-			@RequestParam("userNo") String userNo
-			){		
-		return musicService.insertPlaylist(playlistName, userNo);
-	}
-	
-	@CrossOrigin(origins = "http://localhost:3000")
-	@GetMapping("/deletePlaylist")
-	public int deletePlaylist(
-			@RequestParam("playlistNo") String playlistNo
-			){		
-		return musicService.deletePlaylist(playlistNo);
-	}
-	
-	@CrossOrigin(origins = "http://localhost:3000")
-	@GetMapping("/selectOneMusic")
-	public Music selectOneMusic(
-			@RequestParam("musicNo") String musicNo
-			){
-		return musicService.selectOneMusic(musicNo);
-	}
-	
-	@CrossOrigin(origins = "http://localhost:3000")
-	@GetMapping("/addPlaylistSong")
-	public int addPlaylistSong(
-			@RequestParam("playlistNo") String playlistNo,			
-			@RequestParam("musicNo") String musicNo
-			){
-		return musicService.addPlaylistSong(playlistNo, musicNo);
-	}
-	
-	@CrossOrigin(origins = "http://localhost:3000")
-	@GetMapping("/removePlaylistSong")
-	public int removePlaylistSong(
-			@RequestParam("playlistNo") String playlistNo,			
-			@RequestParam("musicNo") String musicNo
-			){
-		return musicService.removePlaylistSong(playlistNo, musicNo);
-	}
-	
-	@CrossOrigin(origins = "http://localhost:3000")
-	@GetMapping("/increaseCount")
-	public int increaseCount(
-			@RequestParam("musicNo") String musicNo
-			){
-		return musicService.increaseCount(musicNo);
-	}
-	
-	@Scheduled(cron = "0 0 * * * ?") // 매시 정각
-	public void resetHourCount() {
-		musicService.resetHourCount();
-		log.info("1시간 조회수가 초기화 되었습니다.");
-	}
-	@Scheduled(cron = "0 0 0 * * ?") // 매일 자정
-	public void resetDailyCount() {
-		musicService.resetDailyCount();
-		log.info("1일 조회수가 초기화 되었습니다.");
-	}
-	
-	@CrossOrigin(origins = "http://localhost:3000")
-	@GetMapping("/searchMusic")
-	public List<Music> searchMusic(
-			@RequestParam("keyword") String keyword
-			){
-		return musicService.searchMusic(keyword);
-	}
-	@CrossOrigin(origins = "http://localhost:3000")
-	@GetMapping("/insertKeyword")
-	public void insertKeyword(
-			@RequestParam("keyword") String keyword
-			){
-		musicService.insertKeyword(keyword);
-	}
-	
-	
-	@CrossOrigin(origins = "http://localhost:3000")
-	@GetMapping("/selectRanking")
-	public List<String> selectRanking(){
-		return musicService.selectRanking();
-	}
-
-
 }
