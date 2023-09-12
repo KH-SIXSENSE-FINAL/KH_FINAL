4e44645a932c6d512cc2aec202d173a1ba7cc067
ajk6757 <129792901+ajk6757@users.noreply.github.com>
2023-09-12 17:45:15 +0900

    채팅 변경

--- FinalEx/demo/src/main/java/com/kh/muzip/chat/dao/ChatDao.java
+++ FinalEx/demo/src/main/java/com/kh/muzip/chat/dao/ChatDao.java
@@ -13,6 +13,9 @@ import com.kh.muzip.chat.vo.ChatRoom;
 import com.kh.muzip.chat.vo.ChatRoomJoin;
 import com.kh.muzip.member.model.vo.Member;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Repository
 public class ChatDao {
 
