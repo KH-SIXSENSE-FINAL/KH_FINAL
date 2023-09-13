aa8eeac24e0b6e1fd16ef5d953bc493f70ad6bba
chaajae <137244332+chaajae@users.noreply.github.com>
2023-09-13 18:11:03 +0900

    Update PaymentHistory.java

--- FinalEx/demo/src/main/java/com/kh/muzip/setting/model/vo/PaymentHistory.java
+++ FinalEx/demo/src/main/java/com/kh/muzip/setting/model/vo/PaymentHistory.java
@@ -10,7 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PaymentHistory {
-	
 	private String paymentNo;
 	private Date paymentDate;
 	private String userNo;
