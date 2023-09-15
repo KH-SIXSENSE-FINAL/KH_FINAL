fb6b67b450b21bde1bf349b276eada370bfe3c93
hwang seul gi <hsgsmart@naver.com>
2023-09-15 17:44:54 +0900

    0915집가기전 커밋

--- FinalEx/demo/src/main/java/com/kh/muzip/admin/controller/AdminController.java
+++ FinalEx/demo/src/main/java/com/kh/muzip/admin/controller/AdminController.java
@@ -140,7 +140,7 @@ public class AdminController {
 
 	    // 정렬과 검색 조건을 포함하여 게시글 목록을 가져옴
 	    ArrayList<Board> list = adminService.selectContentList(pi, searchTerm, searchType, sortBy);
-
+	    
 	    
 	    return ResponseEntity.ok().body(list);
 	}
@@ -210,7 +210,7 @@ public class AdminController {
 		// 정렬과 검색 조건을 포함하여 게시글 목록을 가져옴
 		ArrayList<Music> list = adminService.selectMusicList(pi, searchTerm, searchType, sortBy);
 
-
+		System.out.println(list);
 		return ResponseEntity.ok().body(list);
 	}
 	
