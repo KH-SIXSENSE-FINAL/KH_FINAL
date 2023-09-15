a9fbe5a5f5658e254d3bbd793bd9891ce2bcf691
hwang seul gi <hsgsmart@naver.com>
2023-09-15 15:32:30 +0900

    충돌 수정

--- FinalEx/demo/src/main/java/com/kh/muzip/admin/model/dao/AdminDao.java
+++ FinalEx/demo/src/main/java/com/kh/muzip/admin/model/dao/AdminDao.java
@@ -13,12 +13,12 @@ import com.kh.muzip.admin.model.vo.PageInfo;
 import com.kh.muzip.board.model.vo.Board;
 import com.kh.muzip.member.model.vo.Member;
 import com.kh.muzip.music.model.vo.Music;
-<<<<<<< HEAD
-=======
+
+
 import com.kh.muzip.setting.model.vo.Contact;
 import com.kh.muzip.setting.model.vo.Genre;
 import com.kh.muzip.setting.model.vo.PaymentHistory;
->>>>>>> dc870cf777995258c274481fc3a04d25263a4512
+
 
 @Repository
 public class AdminDao {
@@ -183,32 +183,27 @@ public class AdminDao {
 
 
 
-<<<<<<< HEAD
-	
-=======
+
 	public int updateAdminReply(HashMap<String, Object> map) {
 		return session.update("admin.updateAdminReply",map);
 	}
->>>>>>> dc870cf777995258c274481fc3a04d25263a4512
 
 
 
 
 
-<<<<<<< HEAD
-	
-=======
+
+
 	public int DeleteContact(HashMap<String, Object> map) {
 		return session.update("admin.DeleteContact",map);
 	}
->>>>>>> dc870cf777995258c274481fc3a04d25263a4512
 
 
 
 
 
-<<<<<<< HEAD
-=======
+
+
 	public int RestoreContact(HashMap<String, Object> map) {
 		return session.update("admin.RestoreContact",map);
 	}
@@ -237,7 +232,7 @@ public class AdminDao {
 
 
 
->>>>>>> dc870cf777995258c274481fc3a04d25263a4512
+
 
 
 	
